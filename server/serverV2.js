2d373a3f9638916a42e4b7062cdd1c9fb7077670
Fara <fmirjalilov98@gmail.com>
2024-01-07 18:19:34 +0100

    server configured

--- server/serverV2.js
+++ server/serverV2.js
@@ -1,11 +1,10 @@
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
-const { error } = require('console');
-require('dotenv').config();
-
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const { error } = require("console");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 4001;
 
@@ -14,26 +13,53 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server"})
+  res.json({ message: "Hello from server" });
 });
 
 const contactEmail = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_ADDRESS,
-        pass: process.env.EMAIL_PASS
-    },
-    authMethod: 'login' // Add this line
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PASS,
+  },
+  authMethod: "login", // Add this line
 });
 
 contactEmail.verify((error) => {
-    if (error) {
-        console.error("Error with email verification:", error);
-    } else {
-        console.log("Server is ready to send emails!");
-    }
+  if (error) {
+    console.error("Error with email verification:", error);
+  } else {
+    console.log("Server is ready to send emails!");
+  }
 });
 
-app.listen( PORT , () => {
-    console.log(`Server is running on port ${PORT}`); 
-})
+app.post(
+  "/api/contact",
+  bodyParser.urlencoded({ extended: false }),
+  (req, res) => {
+    const name = req.body.firstName + req.body.lastName;
+    const email = req.body.email;
+    const message = req.body.message;
+    const phone = req.body.phone;
+    const mail = {
+      from: name,
+      to: process.env.EMAIL_ADDRESS,
+      subject: "Interview",
+      html: `<p>Name: ${name}</p>
+        <p>Email: ${email}</p>
+        <p>Phone: ${phone}</p>
+        <p>Message: ${message}</p>`,
+    };
+    contactEmail.sendMail(mail, (error) => {
+      if (error) {
+        res.json(error);
+      } else {
+        res.json({ code: 200, status: "Message sent" });
+      }
+    });
+  }
+);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

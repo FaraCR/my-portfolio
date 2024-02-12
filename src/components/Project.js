a64f974f5c1f2ed6a7e1f368d763fa4dff248dcb
Fara <fmirjalilov98@gmail.com>
2024-02-12 15:30:20 +0100

    layout changes

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,17 +3,25 @@ import "../css/Projects.css";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCards from "./ProjectCards";
 import CardImg from "../logos/greyImg.png";
-import Jammming from '../logos/jamming.png';
+import Jammming from "../logos/jamming.png";
+import WeatherIcon from '../logos/weather.png';
 
 function Project() {
   const projects = [
     {
       title: "Jammming",
-      description: "My first Portfolio project from Codeacademy. Using Spotify API it allows the user to search for songs, create custom playlists and save them in the user's Spotify account",
+      description:
+        "My first Portfolio project from Codeacademy. Using Spotify API it allows the user to search for songs, create custom playlists and save them in the user's Spotify account",
       imgUrl: Jammming,
-      projectLink:"https://jammmingbyfara.netlify.app"
+      projectLink: "https://jammmingbyfara.netlify.app",
     },
     {
+      title: "Weather App",
+      description: "The Weather App is a simple web application that allows users to search for weather information based on location. It utilizes the OpenWeatherMap API to fetch real-time weather data and displays it in a user-friendly interface.",
+      imgUrl: WeatherIcon,
+      projectLink: "weatherappbyfara.netlify.app"
+    },
+    /*{
       title: "Project",
       description: "Design & Development",
       imgUrl: CardImg,
@@ -22,7 +30,7 @@ function Project() {
       title: "Project",
       description: "Design & Development",
       imgUrl: CardImg,
-    },
+    }*/
   ];
 
   return (
@@ -33,7 +41,8 @@ function Project() {
             <h2>My Projects</h2>
             <p>
               {" "}
-              Here are some projects i have made so far. Feel free to test them out.{" "}
+              Here are some projects i have made so far. Feel free to test them
+              out.{" "}
             </p>
           </Col>
         </Row>
--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -3,7 +3,7 @@ import { Col } from "react-bootstrap";
 
 function ProjectCards({ title, description, imgUrl, projectLink }) {
   return (
-    <Col xs={12} md={12} xl={12} className="d-flex justify-content-center">
+    <Col xs={12} md={6} xl={6} className="d-flex justify-content-center">
       <a href={projectLink} target="_blank" rel="noopener noreferrer">
         <div className="proj-imgbx">
           <img src={imgUrl} alt="Fara's project" width="500" height="300" />
